2538701ceda549f84be44549778546e732c14414
Nico <531521711@qq.com>
2024-08-04 19:24:32 +0800

    Delete uniCloud-alipay directory
    delete cloud related

--- uniCloud-alipay/cloudfunctions/common/utils/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	transformToCamelCase:function(obj) {
-		return Object.keys(obj).reduce((acc, key) => {
-			const camelCaseKey = key.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
-			acc[camelCaseKey] = obj[key];
-			return acc;
-		}, {});
-	}
-}
